81b69a5ce823c00fd4956040a32bdca6c91fa21f
Kar-majue <majuekar@gmail.com>
2023-06-01 17:21:22 -0400

    add backend file

---
+++ backend/ESE_metrics.py
@@ -0,0 +1,19 @@
+from model_tool import *
+
+class Economic():
+
+
+class Social():
+
+class Environmental():
+
+
+
+#--------------------------------------------#
+# Test
+#--------------------------------------------#
+def test():
+
+
+if __name__ == '__main__':
+    test()
